94a1a8363a183c0196fe4ea6661a51d82fc29fd2
ypo-GitHub <69129076+ypo-GitHub@users.noreply.github.com>
2022-01-14 11:27:55 +0800

    Try to build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+#[macro_use]
+use log::*;
+
+use uefi::prelude::*;
+
+#[entry]
+fn efi_main(handle: Handle, system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&system_table).expect_success("failed to initialize utilities");
+    info!("Hello UEFI!");
+    loop {}
 }
